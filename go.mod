3edad9922383950ab109d5be5de787ca057abd96
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2023-02-06 17:32:04 -0300

    Add lib to get doenv variables

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/PedroDalpa/primeiro-crud-golang
+
+go 1.18
+
+require github.com/joho/godotenv v1.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(os.Getenv("TEST"))
+}
